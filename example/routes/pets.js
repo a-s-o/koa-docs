fcc991d730834d0a50386c691fb9d0576bc12298
Amandeep <amandeep@firmhub.ca>
2015-11-10 17:06:17 -0500

    Better example

--- example/routes/pets.js
+++ example/routes/pets.js
@@ -78,7 +78,7 @@ const getPetByStatus = {
             .min(1)        // At least 1 should be provided
             .single()      // If only one is provided, wrap it in an array
       },
-      output: t.array().items(Pet)
+      output: t.array().items(Pet.requiredKeys('id', 'status'))
    },
    *handler () {
       const query = this.request.query;
