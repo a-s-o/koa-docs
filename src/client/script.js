2b5eb2a60b932785e529f7e35f83c2dda8e45640
Amandeep <amandeep@firmhub.ca>
2015-11-10 17:45:13 -0500

    Remove es6 features from front-end script

--- src/client/script.js
+++ src/client/script.js
@@ -1,11 +1,12 @@
 'use strict';
 /* globals $ */
+/* eslint no-var: 0, vars-on-top: 0 */
 
-$('.switch-theme.dropdown-menu').on('click', function changeThem (evt) {
-   const theme = $(evt.target).attr('theme');
+$('.switch-theme.dropdown-menu').on('click', function changeTheme (evt) {
+   var theme = $(evt.target).attr('theme');
    if (typeof theme !== 'string' || !theme) return;
 
-   const themeUrl = theme === 'bootstrap' ?
+   var themeUrl = theme === 'bootstrap' ?
       'bootstrap/3.3.5/css/bootstrap-theme.min.css' :
       'bootswatch/3.3.5/' + theme + '/bootstrap.min.css';
 
